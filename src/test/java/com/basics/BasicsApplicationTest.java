1494d30ce8a40a34a675756b61b0f0bfbcbcbd38
mamgeorge <mamgeorge@yahoo.com>
2020-07-08 22:44:25 -0400

    update tests

--- src/test/java/com/basics/BasicsApplicationTest.java
+++ src/test/java/com/basics/BasicsApplicationTest.java
@@ -1,9 +1,19 @@
 package com.basics;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
 public class BasicsApplicationTest {
 	//
-    public static void main( String[] args ) {
+    public static void main( String[] args ) { System.out.println( "\u001b[32;1m" + "BasicsApplicationTest.main" + "\u001b[0m" ); }
+
+	@Test
+	public void sample( ) { 
 		//
-		System.out.println( "\u001B[31mHELLO BasicsApplicationTest" );
-    }
+		System.out.println( "\u001b[32;1m" + "BasicsApplicationTest.sample" + "\u001b[0m" ); 
+	}
 }
