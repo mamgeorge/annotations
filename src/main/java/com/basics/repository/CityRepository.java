bc77fae1ce2903aebb1d62486bbf1f08b2b035ac
mamgeorge <mamgeorge@yahoo.com>
2021-05-26 08:34:35 -0400

    notes added

--- src/main/java/com/basics/repository/CityRepository.java
+++ src/main/java/com/basics/repository/CityRepository.java
@@ -4,5 +4,4 @@ import com.basics.model.City;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CityRepository extends CrudRepository<City, Long> { }
+@Repository public interface CityRepository extends CrudRepository<City, Long> { }
